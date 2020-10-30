f7ec46aabba62cb751ea7aea051170c52edfb23d
Camila GP <camila.frangp@gmail.com>
2020-10-30 01:15:00 -0300

    remove eslintrc

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 /** @format */
 
 import React from 'react';
-import cx from 'classnames';
 import ListProducts from './modules/ListProducts/ListProducts';
-
 import './App.scss';
 
 const App = () => (
